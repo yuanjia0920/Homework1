5e46970ddc9e004fe6f585251b223eea96c1cf21
yuanjia0920 <yuanjia0920@gmail.com>
2017-02-25 12:17:05 -0800

    JiaYuanHomework1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void EnableUxSubmit()
+        {
+            if (!string.IsNullOrEmpty(uxName.Text) && !string.IsNullOrEmpty(uxPassword.Text))
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else uxSubmit.IsEnabled = false;
+        }            
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableUxSubmit();
+        }
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableUxSubmit();
+        }
+    }
+}
